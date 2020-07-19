4b0adb9f1a06eab08c71390018ddfb5482748990
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-07-19 14:33:58 -0300

    Alteração para lingua HTML
    Foi adicionado as tags para HTML nas páginas de Menu e do Rodapé

--- site/paginas/rodape.php
+++ site/paginas/rodape.php
@@ -1,3 +1,8 @@
-<div class="rodape">
-	<br>© Copyright 2020<br>Todos os Direitos Reservados<br><br>
-</div>
+<!doctype html>
+<html>
+	<body>
+		<div class="rodape">
+			<br>© Copyright 2020<br>Todos os Direitos Reservados<br><br>
+		</div>
+	</body>
+</html>
