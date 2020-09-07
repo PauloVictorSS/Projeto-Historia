f6a05bb1ba6946053e5eacf0dca96c1dae784f75
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-09-07 07:53:41 -0300

    Verificação Painel
    Verificação de login caso o usuário seja um admin

--- php/user/usuario_logando.php
+++ php/user/usuario_logando.php
@@ -15,21 +15,34 @@
 
 		$senhamd5 = md5($senha);
 
-		$consulta = "select login, senha, id_mestre, id from usuarios where login = '$login' and senha = '$senhamd5'";
+		$consulta1 = "SELECT login, senha, id FROM usuarios WHERE login = '$login' AND senha = '$senhamd5'";
+		$consulta2 = "SELECT * FROM `admin.usuarios` WHERE login = '$login' AND senha = '$senha'";
 
-		$result = mysqli_query($conexao, $consulta);
+		$result1 = mysqli_query($conexao, $consulta1);
+		$result2 = mysqli_query($conexao, $consulta2);
 
 		//Verifica se houve algum resultado com o login e senha iguais
-		if(mysqli_num_rows($result) == 1){
+		if(mysqli_num_rows($result1) == 1){
 			$_SESSION['login'] = $login;
-			 
 
-			while($n = mysqli_fetch_array($result)){
+			while($n = mysqli_fetch_array($result1)){
 
 				$_SESSION['status_login'] = 1;
 				$_SESSION['id_usuario'] = $n['id'];
 
 				$url = INCLUDE_PATH.'area-do-usuario';
+
+				header("location: $url");
+			}
+		}
+		elseif(mysqli_num_rows($result2) == 1){
+			$_SESSION['login'] = $login;
+
+			while($n = mysqli_fetch_array($result2)){
+
+				$_SESSION['type_usuario'] = $n['type'];
+
+				$url = INCLUDE_PATH_PANEL;
 
 				header("location: $url");
 			}
