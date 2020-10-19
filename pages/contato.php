17db1ccc656d910bef1f1093f59cb63e52c669f5
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-10-19 16:36:22 -0300

    Enviar Email
    Edição no formulário para o envio de e-mail e backend para o mesmo

--- pages/contato.php
+++ pages/contato.php
@@ -1,13 +1,15 @@
 <main>
 	<section class="contato">
+    <?php  include "php/enviar_email.php";  ?>
 		<div class="box-contato">
 			<div class="center">
 				<h1>Entre em contato</h1>
 				<form action="<?php echo INCLUDE_PATH; ?>contato" method="POST">
 					<h2>Envie um e-mail para nós expondo sugestões</h2>
 					<input type="text" name="nome" placeholder="Nome...">
-					<input type="text" name="e-mail" placeholder="E-mail...">
+					<input type="text" name="email" placeholder="E-mail...">
 					<textarea name="sugestao" placeholder="Sua sugestão aqui..."></textarea>
+          <button name="action" value="1" type="submit">Enviar</button>
 				</form>
 			</div>
 		</div>
---
+++ php/enviar_email.php
@@ -0,0 +1,56 @@
+<?php
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    if(isset($_POST["action"])){
+
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $mens = str_replace("\n", "<br>",$_POST["sugestao"]);
+
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            //Server settings
+            //$mail->SMTPDebug = 3;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'paulovictorsantos0@gmail.com';                     // SMTP username
+            $mail->Password   = 'zcnnckhjposdwziy';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom("$email", "$nome");
+            $mail->addAddress('paulovictorsantos0@gmail.com');
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Sugestão ou dúvida de '.$nome;
+            $mail->Body    = "<br>Nome: $nome<br>Email: $email<hr>$mens";
+            $mail->AltBody = "$mens";
+
+            $mail->send();
+            echo "<div class='mensagem green'>E-mail enviado com sucesso</div>";
+        } catch (Exception $e) {
+            echo "<div class='mensagem red'>Erro ao enviar o e-mail, por favor tente mais tarde</div>";
+        }
+    }
+?>
