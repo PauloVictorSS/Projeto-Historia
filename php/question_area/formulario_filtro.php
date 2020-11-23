c23c13852d34997668a14c698c914c1be686e947
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2020-11-23 18:24:37 -0300

    Maneira de mostrar os anos nos formulários
    Mudança na maneira de mostrar os anos nos formulários, agora está de maneira dinâmica

--- php/question_area/formulario_filtro.php
+++ php/question_area/formulario_filtro.php
@@ -21,28 +21,26 @@
 	echo "</select>";
 				
 	echo "<select name='ano' id='ano-input' class='w33'>
-				<option value=''>Ano</option>
-				<option value='2020'>2020</option>
-				<option value='2019'>2019</option>
-				<option value='2018'>2018</option>
-				<option value='2017'>2017</option>
-				<option value='2016'>2016</option>
-				<option value='2015'>2015</option>
-				<option value='2014'>2014</option>
-				<option value='2013'>2013</option>
-				<option value='2012'>2012</option>
-				<option value='2011'>2011</option>
-				<option value='2010'>2010</option>
-				<option value='2009'>2009</option>
-			</select>";
+				<option value=''>Ano</option>";
+
+	setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+	$ano = date('Y');
+	$ano++;
+
+	while($ano > 1900){
+		echo "<option value='$ano'>$ano</option>";
+		$ano = $ano - 1;
+	}
+
+	echo"</select>";
 
 
 	echo "<select name='qtd_quest' id='qtd_quest-input' class='w33'>
 				<option value=''>Mostrar</option>
-				<option value='4'>4 Elemtentos</option>
-				<option value='6'>6 Elemtentos</option>
-				<option value='10'>10 Elemtentos</option>
-				<option value='20'>20 Elemtentos</option>
+				<option value='4'>4 Questões</option>
+				<option value='6'>6 Questões</option>
+				<option value='10'>10 Questões</option>
+				<option value='20'>20 Questões</option>
 			</select>";
 
 	echo "<select name='tema' id='tema-input'><option value=''>Temas</option>";
