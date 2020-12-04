7e3cd5728b774a6584870a2710d4b096e0ed04f9
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2020-12-04 16:40:39 -0300

    Área do Usuário - Questões p/ tema
    Agora os temas que o aluno já resolveu são separados pelas respectivas matérias

--- php/user/usuario_dadosquestoes.php
+++ php/user/usuario_dadosquestoes.php
@@ -2,14 +2,28 @@
 
     $id = $_SESSION['id_usuario'];
 
-    $consulta = "select sub_tema.descricao, COUNT(questoes.id_sub_tema) as 'qtd' from resolucao, questoes, sub_tema where id_usuario = $id and resolucao.id_questao = questoes.id and questoes.id_sub_tema = sub_tema.id GROUP BY questoes.id_sub_tema";
+    $consulta_materia = "select * from materia";
+    $result_materia = mysqli_query($conexao, $consulta_materia);
 
-    $result = mysqli_query($conexao, $consulta);
+    while($materia = mysqli_fetch_array($result_materia)){
 
-    while ($n = mysqli_fetch_array($result)){	
-        
-        echo "<p class='temas'><b>".$n['descricao'].":</b> ".$n['qtd']."</p>";
+        $id_materia = $materia["id"];
 
+        $consulta_tema = "select sub_tema.descricao, COUNT(questoes.id_sub_tema) as 'qtd' from resolucao, questoes, sub_tema where id_usuario = $id and resolucao.id_questao = questoes.id and questoes.id_sub_tema = sub_tema.id and resolucao.id_materia = $id_materia GROUP BY questoes.id_sub_tema";
+
+        $result_tema = mysqli_query($conexao, $consulta_tema);
+
+        if(mysqli_num_rows($result_tema) > 0){
+            echo "<div class='temas_p_materia'><h3>".$materia['nome']."</h3>";
+
+            while ($tema = mysqli_fetch_array($result_tema)){	
+
+                echo "<p class='temas'><b>".$tema['descricao'].":</b> ".$tema['qtd']."</p>";
+
+            }
+
+            echo "</div>";
+        }
     }
 
 ?>
