3a4e6804a107fc440146c9e770efafae94495ffe
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2020-12-06 08:32:09 -0300

    Alteração da classe Questao
    Adição dos métodos filterQuestions(), totalResultFilter() e resetFilter() para trocar o sistema de filtro da Área de Questão para Orientação a Objeto

--- class/Questao.php
+++ class/Questao.php
@@ -2,6 +2,12 @@
 
     class Questao{
 
+        private static $tema = "";
+        private static $ano = "";
+        private static $partenome = "";
+        private static $vestibular = "";
+        private static $materia = "";
+
         public static function selectExams(){
 
             $sql = MySql::getConnect()->prepare("SELECT * FROM `vestibular` ORDER BY descricao");
@@ -9,9 +15,9 @@
 			return $sql->fetchAll();
         }
 
-        public static function selectThemes($materia){
+        public static function selectThemes(){
 
-            $sql = MySql::getConnect()->prepare("SELECT * FROM `sub_tema` WHERE id_materia = $materia ORDER BY descricao");
+            $sql = MySql::getConnect()->prepare("SELECT * FROM `sub_tema` ORDER BY descricao");
 			$sql->execute();
 			return $sql->fetchAll();
         }
@@ -32,6 +38,50 @@
             $sql->execute(array($enunciado, $pergunta, $quest_a, $quest_b, $quest_c, $resp_a, $resp_b, $resp_c, $tema, $vest, $ano, $tipo, $materia));
 
             return $sql->rowCount();
+        }
+
+        public static function filterQuestions($tema, $ano, $partenome, $vestibular, $materia){
+            self::$tema = $tema;
+            self::$ano = $ano;
+            self::$partenome = $partenome;
+            self::$vestibular = $vestibular;
+            self::$materia = $materia;
+        }
+
+        public static function resultFilter($inicio, $total_reg){
+
+            $tema = self::$tema;
+            $ano = self::$ano;
+            $enunciado = self::$partenome;
+            $vestibular = self::$vestibular;
+            $materia = self::$materia;
+
+            $sql = MySql::getConnect()->prepare("SELECT vestibular.`descricao` AS `vestibular`, questoes.`ano`, questoes.`especial`, questoes.`imagem`, sub_tema.`descricao` AS `subtema`, questoes.`enunciado`, questoes.`alternativa_a`, questoes.`alternativa_b`, questoes.`alternativa_c`, questoes.`alternativa_d`,questoes.`alternativa_e`, questoes.`alternativa_certa`, questoes.`explicacao`, questoes.`pergunta`, questoes.`id`, questoes.`tipo`, materia.`nome` FROM questoes, materia, vestibular, sub_tema WHERE questoes.`id_vestibular` = vestibular.`id` and questoes.`id_sub_tema` = sub_tema.`id` and questoes.`id_materia` = materia.`id` AND (questoes.enunciado LIKE '%$enunciado%' or questoes.pergunta LIKE '%$enunciado%') AND (questoes.ano LIKE '%$ano%' AND vestibular.descricao LIKE '%$vestibular%' AND sub_tema.descricao LIKE '%$tema%' and materia.nome LIKE '%$materia%') LIMIT $inicio, $total_reg");
+            
+			$sql->execute();
+			return $sql->fetchAll();
+        }
+
+        public static function totalResultFilter(){
+
+            $tema = self::$tema;
+            $ano = self::$ano;
+            $enunciado = self::$partenome;
+            $vestibular = self::$vestibular;
+            $materia = self::$materia;
+
+            $sql = MySql::getConnect()->prepare("SELECT vestibular.`descricao` AS `vestibular`, questoes.`ano`, questoes.`especial`, questoes.`imagem`, sub_tema.`descricao` AS `subtema`, questoes.`enunciado`, questoes.`alternativa_a`, questoes.`alternativa_b`, questoes.`alternativa_c`, questoes.`alternativa_d`,questoes.`alternativa_e`, questoes.`alternativa_certa`, questoes.`explicacao`, questoes.`pergunta`, questoes.`id`, questoes.`tipo`, materia.`nome` FROM questoes, materia, vestibular, sub_tema WHERE questoes.`id_vestibular` = vestibular.`id` and questoes.`id_sub_tema` = sub_tema.`id` and questoes.`id_materia` = materia.`id` AND (questoes.enunciado LIKE '%$enunciado%' or questoes.pergunta LIKE '%$enunciado%') AND (questoes.ano LIKE '%$ano%' AND vestibular.descricao LIKE '%$vestibular%' AND sub_tema.descricao LIKE '%$tema%' and materia.nome LIKE '%$materia%')");
+            
+			$sql->execute();
+			return $sql->fetchAll();
+        }
+
+        public static function resetFilter(){
+            self::$tema = "";
+            self::$ano = "";
+            self::$partenome = "";
+            self::$vestibular = "";
+            self::$materia = "";
         }
 
     }
