12da02578e76917fd8bd52b453296839a0dbb809
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2020-12-19 13:31:13 -0300

    Área do Usuário - Funções em OO
    Agora as funcionalidades da área do usuários são feitas com Orientação a Objetos, então houve algumas modificações

--- pages/area_do_usuario.php
+++ pages/area_do_usuario.php
@@ -1,10 +1,8 @@
 <?php 
     include_once("../config.php");
-    include_once("../include/start_conexao.php");
     
-	if(!isset($_SESSION['status_login'])){
+	if(!isset($_SESSION['status_login']))
 		header("Location:".INCLUDE_PATH);
-	}
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +29,6 @@
         //Importar o CSS da página caso exista
         if(file_exists('public/css/page_'.$explode[0].'.css'))
             echo'<link href="'.INCLUDE_PATH.'public/css/page_'.$explode[0].'.css" rel="stylesheet">';
-
     ?>
 
 </head>
@@ -48,8 +45,18 @@
                 <div class="clear"></div>
             </div>
             <div class="box-links">
-                <table class="dados-pessoais">				
-                    <?php  include_once("../php/user/usuario_dadospessoais.php");  ?>
+                <table class="dados-pessoais">
+
+                    <?php  
+    
+                        $user = User::getUserInfs($_SESSION["id_usuario"]);
+
+                        echo "<p><b>Nome:</b><br>".$user[0][0]."</p><br>";
+                        echo "<p><b>E-mail:</b><br>".$user[0][1]."</p><br>";
+                        echo "<p><b>Data de Nascimento:</b><br>".$user[0][2]."</p><br>";
+                        echo "<p><b>Rede:</b><br>".$user[0][3]."</p><br>";
+                        echo "<p><b>Escolariedade:</b><br>".$user[0][4]."</p>";
+                    ?>
                 </table>
             </div>
             <div class="links">
@@ -66,11 +73,29 @@
                     <h2>Quantidade de questões feitas p/ tema</h2>
                     <div class="dados-questoes">
                         
-                        <?php  include_once("../php/user/usuario_dadosquestoes.php");		?>
+                        <?php  
 
-                        <br><br><a href="<?php echo INCLUDE_PATH;?>area_de_questoes" class="link"><h3>Ir resolver mais questões!</h3></a>
+                            $materias = Adm::getSubject();
+
+                            foreach ($materias as $key => $materia) {
+                                
+                                $result = User::resolvedQuestionTheme($materia['id'], $_SESSION['id_usuario']);
+
+                                if(count($result) > 0){
+
+                                    echo "<div class='temas_p_materia'><h3>".$materia['nome']."</h3>";
+
+                                    foreach ($result as $key => $tema) 
+                                        echo "<p class='temas'><b>".$tema['descricao'].":</b> ".$tema['qtd']."</p>";
+                                    
+                                    echo "</div>";
+                                }else
+                                    echo"<div class='temas_p_materia'><h3>Opa, você ainda não resolveu nenhuma questão!</h3></div>";
+                            }
+                        ?>
+                        <br><br>
+                        <a href="<?php echo INCLUDE_PATH;?>area_de_questoes" class="link"><h3>Ir resolver mais questões!</h3></a>
                     </div>
-                    
                     <div class="clear"></div>
                 </section>
             </div>
