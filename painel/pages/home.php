f645209f8bf86df011a87a2b13cf5c207466446f
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2020-12-19 14:03:35 -0300

    Mudança da função para Orientação a Objetos
    Pequena função que recuperava o nome da materia com base em seu ID agora está utilizando OO

--- painel/pages/home.php
+++ painel/pages/home.php
@@ -1,10 +1,12 @@
 <?php
 
-    $result = mysqli_query($conexao, "select nome from materia where id = $materia");
-    $materia = mysqli_fetch_array($result);
+    $materias = Adm::getSubject();
 
-    $nome_materia = $materia[0];
-
+    foreach ($materias as $key => $materia) {
+        
+        if($materia['id'] == $_SESSION['materia_prof'])
+            $nome_materia = $materia['nome'];
+    }
 ?>
 <div class="box-content">
     <h1>Bem vindo a área administrativa</h1><br><br>
