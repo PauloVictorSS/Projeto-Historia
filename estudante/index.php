78a2aad2a0ff9356c807f4b213d9a7f9b506cb48
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2021-01-04 11:01:00 -0300

    Sistema de URLs dinâmicas
    Adição de um sistema de URLs dinâminas na "Área do Usuário" para alterar o conteúdo central de acordo com a URL enviada via GET para o navegador

--- estudante/index.php
+++ estudante/index.php
@@ -2,7 +2,13 @@
     include_once("../config.php");
     
 	if(!isset($_SESSION['status_login']))
-		header("Location:".INCLUDE_PATH);
+        header("Location:".INCLUDE_PATH);
+        
+    if(isset($_GET['loggout'])){
+        header("Location:".INCLUDE_PATH."php/deslogar.php");
+    }
+
+    $user = User::getUserInfs($_SESSION["id_usuario"]);
 ?>
 
 <!DOCTYPE html>
@@ -21,16 +27,15 @@
     <?php
 
         //Recuperando a url selecionada
-        $url = isset($_GET['url']) ? $_GET['url'] : 'questoes_feitas';
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
         //Separando a url de possíveis parâmetros
         $explode = explode("-", $url);
 
         //Importar o CSS da página caso exista
-        if(file_exists('public/css/page_'.$explode[0].'.css'))
+        if(file_exists('css/page_'.$explode[0].'.css'))
             echo'<link href="'.INCLUDE_PATH_ESTUDANTE.'css/page_'.$explode[0].'.css" rel="stylesheet">';
     ?>
-
 </head>
 <body>
 
@@ -38,56 +43,44 @@
         <div class="menu-wraper">
             <div class="box-usuario">
                 <div class="logo"><a href="<?php echo INCLUDE_PATH; ?>"><img src="<?php echo INCLUDE_PATH; ?>public/images/logo.ico"></a></div>
-                <h2>Dados Pessoais:</h2>
+                <div class="nome-usuario">
+                    <p><b>Nome: </b><?php echo $user[0][0]; ?></p>
+                </div>
                 <div class="botao-menu-mobile">
                     <i class="fa fa-bars" aria-hidden="true"></i>
                 </div>
                 <div class="clear"></div>
             </div>
             <div class="box-links">
-                <table class="dados-pessoais">
-
-                    <?php  
-    
-                        $user = User::getUserInfs($_SESSION["id_usuario"]);
-
-                        echo "<p><b>Nome:</b><br>".$user[0][0]."</p><br>";
-                        echo "<p><b>E-mail:</b><br>".$user[0][1]."</p><br>";
-                        echo "<p><b>Data de Nascimento:</b><br>".$user[0][2]."</p><br>";
-                        echo "<p><b>Rede:</b><br>".$user[0][3]."</p><br>";
-                        echo "<p><b>Escolariedade:</b><br>".$user[0][4]."</p>";
-                    ?>
-                </table>
+                <div class="itens">
+                    <a href="<?php echo INCLUDE_PATH_ESTUDANTE ?>">Home</a>
+                    <a href="<?php echo INCLUDE_PATH_ESTUDANTE ?>questoes_feitas">Questões resolvidas</a>
+                    <a href="<?php echo INCLUDE_PATH_ESTUDANTE ?>alterar_dados">Alterar dados pessoais</a>
+                </div>  
             </div>
             <div class="links">
-                <a href="<?php echo INCLUDE_PATH ?>" class="left link"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
-                <a href="../php/deslogar.php" id="sair" class="right link">Deslogar <i class="fa fa-sign-out" aria-hidden="true"></i></a>
+                <a href="<?php echo INCLUDE_PATH ?>" class="loggout left"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+                <a href="<?php echo INCLUDE_PATH_ESTUDANTE ?>?loggout" class="loggout right">Sair <i class="fa fa-sign-out"></i></a>
                 <div class="clear"></div>
             </div>
         </div>
     </div>
     <section class="content">
-        <div class="box-content">
-            <div class="center">
-                <section>
 
-                <?php
+        <?php
 
-                    //Verificando de há algum parâmetro na
-                    if(count($explode) > 1)
-                        $pagina =  $explode[1];
-                    else
-                        $pagina = 1;
+            //Verificando de há algum parâmetro na
+            if(count($explode) > 1)
+                $pagina =  $explode[1];
+            else
+                $pagina = 1;
 
-                    //Verificando se a url escolhida existe
-                    if(file_exists('pages/'.$explode[0].'.php'))
-                        include_once('pages/'.$explode[0].'.php');
-                    else
-                        header("Location: pages/erro404.php");
-                ?>
-                </section>
-            </div>
-        </div>
+            //Verificando se a url escolhida existe
+            if(file_exists('pages/'.$explode[0].'.php'))
+                include_once('pages/'.$explode[0].'.php');
+            else
+                header("Location: pages/erro404.php");
+        ?>
     </section>
 
     <script src="<?php echo INCLUDE_PATH ?>public/js/jquery.js"></script>
