e895011b17c7664be1f906f7b63f7a0d4f8faaa9
Paulo Victor de Souza Santos <50722782+PauloVictorSS@users.noreply.github.com>
2021-01-09 09:39:17 -0300

    Função de alterar a senha para ADMs
    Adição de um formuláro para professores alterarem suas senhas no sistema

--- painel/pages/home.php
+++ painel/pages/home.php
@@ -7,12 +7,48 @@
         if($materia['id'] == $_SESSION['materia_prof'])
             $nome_materia = $materia['nome'];
     }
+
+    if(isset($_POST["submit"])){
+
+        $senha_atual = hash("sha512", $_POST["pass"]);
+
+        if($senha_atual == $_SESSION["prof_admin"]){
+            if($_POST["newpass"] == $_POST["conf_newpass"]){
+
+                if(Painel::newPass($_POST["newpass"], $_SESSION["id"]) == 1)
+                    echo "<div class='mensagem green'>Senha alterada com sucesso</div>";
+                else
+                    echo "<div class='mensagem red'>Houve algum erro ao alterar a senha, por favor contate-nos e tente mais tarde</div>";
+            }
+            else
+                echo"<div class='mensagem red'>Campo de 'Nova senha' e 'Confirme sua nova senha' estão diferentes</div>";
+        }
+        else    
+            echo"<div class='mensagem red'>Senha atual errada</div>";
+    }
 ?>
 <div class="box-content">
-    <h1>Bem vindo a área administrativa</h1><br><br>
-    <h3>Matéria de <?php echo $nome_materia; ?></h3>
+    <h1>Bem vindo a área administrativa</h1><br>
+    <h2>Matéria de <?php echo $nome_materia; ?></h2>
 
     <div class="dados">
-        
+        <h3>Alterar senha</h3>
+        <form action="<?php echo INCLUDE_PATH_PAINEL; ?>" method="POST">
+
+        <div class="text-box">
+            <label for="pass">Senha atual</label>
+            <input type="password" name="pass" id="pass" class="informacoes" placeholder="Senha atual" maxlength="16" required>
+        </div>
+        <div class="text-box">
+            <label for="newpass">Nova senha</label>
+            <input type="password" name="newpass" id="newpass" class="informacoes" placeholder="Nova senha" maxlength="16" required>
+        </div>
+        <div class="text-box">
+            <label for="conf_newpass">Confirme sua nova senha</label>
+            <input type="password" name="conf_newpass" id="conf_newpass" class="informacoes" placeholder="Confirme sua nova senha" maxlength="16" required>
+        </div>
+
+        <button type="submit" name="submit" value="action">Alterar Senha</button>
+        </form>
     </div> 
 </div>
